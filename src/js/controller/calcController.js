e698c37ab5425e81a93c431c1bbc7134cfa4d5b2
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-06-26 23:48:13 -0300

    start button events
    start button events

--- src/js/controller/calcController.js
+++ src/js/controller/calcController.js
@@ -7,6 +7,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize() {
@@ -49,6 +50,16 @@ class CalcController {
 
     set displayDate(value) {
         this._dateEl.innerHTML = value;
+    }
+
+    initButtonsEvents() {
+        //Get all the children of buttons.
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+        buttons.forEach((btn, index) => {
+            btn.addEventListener('click', e => {
+
+            });
+        });
     }
 
     setDisplayDateTime() {
