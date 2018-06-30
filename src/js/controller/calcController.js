cd5cc10f6c48f27d07511e4a88f88e1b5ae98788
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-06-30 12:03:43 -0300

    operations
    operations

--- src/js/controller/calcController.js
+++ src/js/controller/calcController.js
@@ -65,14 +65,31 @@ class CalcController {
         return this._operation[this._operation.length - 1];
     }
 
+    setLastOperation(value) {
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    isOperator(value) {
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
+    }
+
     addOperation(value) {
         //String
         if (isNaN(this.getLastOperation())) {
-            
+            if (this.isOperator(value)) {
+                this.setLastOperation(value);
+            }
+            else if (isNaN(value)) {
+
+            }
+            else {
+                this._operation.push(value);
+            }
         }
         //Number
         else {
-            
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
         }
 
         this._operation.push(value);
@@ -91,18 +108,24 @@ class CalcController {
                 this.clearEntry();
                 break;
             case 'soma':
+                this.addOperation('+');
                 break;
             case 'subtracao':
+                this.addOperation('-');
                 break;
             case 'divisao':
+                this.addOperation('/');
                 break;
             case 'multiplicacao':
+                this.addOperation('*');
                 break;
             case 'porcento':
+                this.addOperation('%');
                 break;
             case 'igual':
                 break;
-            case '.':
+            case 'ponto':
+                this.addOperation('.');
                 break;
             case '0':
             case '1':
