1fc49c00b120e50ddfbd5df65d86870dcf1c59d6
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-07-01 22:15:06 -0300

    Audio
    Audio on click. Activated after two clicks on AC

--- src/js/controller/calcController.js
+++ src/js/controller/calcController.js
@@ -1,6 +1,8 @@
 class CalcController {
 
     constructor() {
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -40,10 +42,28 @@ class CalcController {
         }, 1000);
 
         this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+            btn.addEventListener('dblclick', e => {
+                this.toggleAudio();
+            });
+        });
+    }
+
+    toggleAudio() {
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio() {
+        if (this._audioOnOff) {
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     initKeyboard() {
         document.addEventListener('keyup', e => {
+            this.playAudio();
             switch (e.key) {
                 case 'Escape':
                     this.clearAll();
@@ -265,6 +285,7 @@ class CalcController {
     }
 
     execBtn(value) {
+        this.playAudio();
         switch (value) {
             case 'ac':
                 this.clearAll();
